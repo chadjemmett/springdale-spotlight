618b6e34cf65009685b7e2e7f613e0ea41b5dedf
Chad Jemmett <chad.jemmett@gmail.com>
2025-01-06 14:51:33 -0600

    Fix the permission problem

--- spotlight_database/settings.py
+++ spotlight_database/settings.py
@@ -25,12 +25,12 @@ SECRET_KEY = "django-insecure-9p!qiol8tb2^gas!@xv#b689iqto)qae$x=ld7=h)tkabp(ci*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cjemmett.pythonanywhere.com']
+ALLOWED_HOSTS = ['cjemmett.pythonanywhere.com', '127.0.0.1']
 
 
 REST_FRAMEWORK = {
 
-        'DEFAULT_PERMISSION_CLASSES': 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        # 'DEFAULT_PERMISSION_CLASSES': 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
 
         }
 
